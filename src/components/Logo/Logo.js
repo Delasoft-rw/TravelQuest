681afe0f43f115cdf04280f15b4a2f2843920c2e
igaimerca <igaimerca@gmail.com>
2023-05-30 12:16:27 +0200

    feat: logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,7 +1,8 @@
-import LogoImage from '../../assets/images/icons/airplane.png';
+import LogoImage from '../../assets/images/icons/logo.png';
 
 const Logo = () => {
     return <img src={LogoImage} alt="Travel Quest" width="200" />;
 };
 
 export default Logo;
+o
